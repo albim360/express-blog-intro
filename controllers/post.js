c6d865e9ab312cfd9125140fc1f7d1dbfb7d5dfc
Alberto <74522184+albim360@users.noreply.github.com>
2023-11-10 16:25:05 +0100

    FEAT add html without separated files

--- controllers/post.js
+++ controllers/post.js
@@ -6,8 +6,37 @@ function getPosts(req, res) {
   if (acceptHeader.includes("application/json")) {
     res.json(posts);
   } else if (acceptHeader.includes("text/html")) {
-    const ul = `<ul>${posts.map(post => `<li>${post.title}</li>`).join("")}</ul>`;
-    res.send(ul);
+    const ul = `<ul>${posts
+      .map(
+        (post) =>
+          `<li><strong>${post.title}</strong> <span style="margin-left: 10px;"> ${post.content}</span> </li>`
+      )
+      .join("")}</ul>`;
+
+    res.send(`
+<html>
+<head>
+<style>
+li {
+  margin: 10px 0;
+  padding: 10px;
+  background-color: #f2f2f2;
+  border-radius: 4px;
+}
+
+strong {
+  font-size: 1.2em;
+  display: block;
+  margin-bottom: 5px;
+}
+</style>  
+</head>
+
+<body>
+${ul}
+</body>
+</html>
+`);
   } else {
     // Gestione dei formati non supportati
     res.status(406).send("Formato non supportato");
